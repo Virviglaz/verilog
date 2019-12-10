e5380a8357f7058d2cce1d388fedbdb56906f602
Pavel Nadein <pavelnadein@gmail.com>
2019-12-10 21:08:39 +0100

    spi_slave: added rx_fifo_ready and tx_fifo_ready outputs
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- spi_slave.v
+++ spi_slave.v
@@ -11,17 +11,22 @@ module SPI_slave #(
 	input wire csn,
 	output reg[BITS-1:0] data_from_master,
 	input wire[BITS-1:0] data_from_slave,
-	output reg ready
+	output wire rx_fifo_ready,
+	output wire tx_fifo_ready
 );
 
 	reg[1:0] sck_change;
 	reg[1:0] csn_change;
+	reg[1:0] rdy_change;
 
 	reg[BITS-1:0] buffer;
+	reg[2:0] bitcnt;
+	wire byte_ready;
 
 	always @(posedge clk) begin
 		sck_change <= csn ? 2'b00 : { sck_change[0], sck };
 		csn_change <= { csn_change[0], csn };
+		rdy_change <= { rdy_change[0], byte_ready };
 	end
 
 	/* MOSI valid at sck rising (SPI_MODE0) */
@@ -31,21 +36,25 @@ module SPI_slave #(
 
 	wire chip_enabled = (csn_change == 2'b10);
 	wire chip_disabled = (csn_change == 2'b01);
+	assign rx_fifo_ready = (rdy_change == 2'b10);
 
 	assign miso = csn ? 1'bz : buffer[BITS-1];
+	assign byte_ready = (bitcnt == 3'b111 && sck_fallingEdge) ? 1'b1 : 1'b0;
+	assign tx_fifo_ready = (bitcnt == 3'b111 && sck_risingEdge) ? 1'b1 : 1'b0;
 
 	always @(posedge clk) begin
 		if (chip_enabled) begin
 			buffer <= data_from_slave;
-			ready <= 1'b0;
-		end
-
-		if (chip_disabled) begin
-			data_from_master <= buffer;
-			ready <= 1'b1;
+			bitcnt <= 3'b000;
 		end
 
 		if (~csn && sck_risingEdge)
 			buffer <= { buffer[BITS-2:0], mosi };
+
+		if (~csn && sck_fallingEdge)
+			bitcnt <= bitcnt + 1'b1;
+
+		if (byte_ready)
+			data_from_master <= buffer;
 	end
 endmodule
